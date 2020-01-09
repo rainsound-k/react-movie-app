d55c35e8616263a6ac42576c863cd0e475b05275
rainsound-k <rainsound128@gmail.com>
2020-01-09 22:43:40 +0900

    Updates

---
+++ precache-manifest.c33461381311b19cd45f349a947961ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf88dba2f3f32e96fc5d215a5969c3a2",
+    "url": "/lotto_front/index.html"
+  },
+  {
+    "revision": "cf9d65c6288000665785",
+    "url": "/lotto_front/static/css/main.a5bc2c52.chunk.css"
+  },
+  {
+    "revision": "3561120460544cde2ca0",
+    "url": "/lotto_front/static/js/2.019d7cd3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/lotto_front/static/js/2.019d7cd3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf9d65c6288000665785",
+    "url": "/lotto_front/static/js/main.39e8e91a.chunk.js"
+  },
+  {
+    "revision": "d607038f645aa3352bb4",
+    "url": "/lotto_front/static/js/runtime-main.7064e276.js"
+  }
+]);
